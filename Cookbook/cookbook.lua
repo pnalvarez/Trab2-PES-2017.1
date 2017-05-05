fc70aeea39b70cab15c5b9ec44c1896ccc8e5116
Pedro Neves Alvarez <pedronalvarez@Pedros-MacBook-Pro.local>
2017-05-04 22:37:47 -0300

    Adicionando o Monolith

--- Cookbook/cookbook.lua
+++ Cookbook/cookbook.lua
@@ -13,7 +13,6 @@ words,stop_words,filter={}
 --AS: o conteudo do arquivo do path foi salvo na variavel data
 function read_file(path_to_file)
 	local f = io.open(path_to_file, "r")
-	assert(f ~= nil, "Path nao vale")
 	data = f:read("*all")
 	f:close()
 end
@@ -23,7 +22,6 @@ end
 --AE: data é uma string nao nula
 --AS: data possui espaco nas posicoes originalmente ocupadas por simbolos alfa-numericos e letras maiusculas por minusculas
 function filter_chars_and_normalize()
-	assert(data ~= nil, "String nula")
     data = data:gsub('%W',' '):lower()
 end
 
@@ -31,7 +29,6 @@ end
 --AE: data é uma string nao nula
 --AS: words foi preenchido com as palavras capturadas
 function scan()
-	assert(data ~= nil, "String nula")
 	local i = data:gmatch("%S+")
 
 	for elem in i do
@@ -39,37 +36,41 @@ function scan()
 	end
 end
 
+--Função que verifica se uma palavra se encontra na tabela
+--AE:O valor deve ser válido e a tabela não vazia
+--AS:Retorna true se a palavra se encontra na tabela e falso se não se encontra
+function isOnList(word, list)
+    for _, wordInList in pairs(list) do
+        if wordInList == word then
+            return true
+        end
+    end
+
+    return false
+end
+
 --remove_stop_words: Funcao que retira as palavras ignoradas da tabela words
 --AE: words nao e nulo e o path para o arquivo stop_words.txt vale
 --AS: As palavras ignoradas foram removidas da tabela words
 function remove_stop_words()
-	assert(words ~= nil, "Tabela words nula")
-	local f = io.open("stop_words.txt", "r")
-	local i = f:read("*all"):gmatch("([^,]+)")
-    stop_words={}
-
-	for elem in i do table.insert(stop_words, elem) end
-	for ascii = 97, 122 do table.insert(stop_words, string.char(ascii)) end
-
-	for index = #words, 1, -1 do
-		for k, stop_word in ipairs(stop_words) do
-			local word = words[index]
-
-			if(word == stop_word) then
-				table.remove(words, index)
-			end
-		end
-	end
+    local stop = assert(io.open("stop_words.txt", 'r')):read('*all'):lower():separa(',')
+    local copy = {}
+    
+    for _, word in pairs(words) do
+        if isOnList(word, stop) == false then
+            table.insert(copy, word)
+        end
+    end
+    words = copy
 end
 
 --frequencies:Funcao que associa numa tabela cada palavra com sua devida frequencia
 --AE:words e uma tabela valida
 --AS:word_freqs e uma tabela que associa cada palavra a sua frequencia
 function frequencies()
-	assert(words ~= nil, "words nulo")
     word_freqs = {}
-    
-    for k, word in ipairs(words) do
+
+    for i, word in ipairs(words) do
         if word_freqs[word] ~= nil then
             word_freqs[word].frequency = word_freqs[word].frequency + 1
         else
@@ -82,15 +83,20 @@ end
 --AE:word_freqs nao e nula
 --AS:word_freqs esta ordenado segundo a frequencia
 function sort()
-	assert(word_freqs ~= nil, "tabela nula")
     frequencies = {}
 
-	for key,elem in pairs(word_freqs) do table.insert(frequencies, elem) end
+	for word,freq in pairs(word_freqs) do table.insert(frequencies, freq) end
 
 	table.sort(frequencies, function(a, b) return a.frequency > b.frequency end)
 
 	word_freqs = frequencies
 end
+
+function print_prefered_words()
+	for i =0,25 do table.insert(filter,word_freqs[i]) end
+	for k,v in pairs(word_freqs) do io.write(v.word.."-"..v.frequency) end
+end
+
 --[[Parte 3: Programa principal--]]
 
 read_file(arg[1])
@@ -99,12 +105,4 @@ scan()
 remove_stop_words()
 frequencies()
 sort()
-
-for i =0,25 do table.insert(filter,word_freqs[i]) end
-for k,v in pairs(word_freqs) do io.write(v.word.."-"..v.frequency) end
-
-
-
-
-
-
+print_prefered_words()
---
+++ monolith.lua
@@ -0,0 +1,62 @@
+--[[Estilo cookbook--]]
+--[[Autor: Pedro N. Alvarez--]]
+
+--[[Parte 1: Declaracao das variaveis globais--]]
+
+data , word_freq,file= nil
+words,stop_words,filter={}
+OnList=false
+
+--Parte 2: Leitura do arquivo parametro e manipulacao interna
+--AE: O arquivo existe e seu path vale
+--AS:O arquivo stop-words.txt foi gerado com as palavras mais frequentes e cada uma mapeada a seu numero de ocorrencias
+file= io.open("../pride-and-prejudice.txt", "r")
+    data = file:read("*all")
+    file:close()
+    data = data:gsub('%W',' '):lower()
+
+    i = data:gmatch("%S+")
+
+    for elem in i do
+       table.insert(words, elem)
+    end
+
+    stop = assert(io.open("stop_words.txt", 'r')):read('*all'):lower():separa(',')
+    local copy = {}
+    
+    for _, word in pairs(words) do
+         for _, wordInList in pairs(list) do
+        if wordInList == word then
+            onList=true
+        end
+    end
+
+    onList =  false
+        if isOnList == false then
+            table.insert(copy, word)
+        end
+    end
+    words = copy
+
+    word_freqs = {}
+
+    for i, word in ipairs(words) do
+        if word_freqs[word] ~= nil then
+            word_freqs[word].frequency = word_freqs[word].frequency + 1
+        else
+            word_freqs[word] = {word = word, frequency = 1}
+        end
+    end
+
+    frequencies = {}
+
+    for word,freq in pairs(word_freqs) do table.insert(frequencies, freq) end
+
+    table.sort(frequencies, function(a, b) return a.frequency > b.frequency end)
+
+    word_freqs = frequencies
+
+  --Parte 3: Imprimir a saida do programa(output)
+
+    for i =0,25 do table.insert(filter,word_freqs[i]) end
+    for k,v in pairs(word_freqs) do io.write(v.word.."-"..v.frequency) end
